b6ef2fa69b5f0ab875aabbd1f48735fd1c30b34e
Shankar Khatri <yes143.shankar@gmail.com>
2019-05-08 19:10:32 -0500

    Add helmet package and require it

--- myApp.js
+++ myApp.js
@@ -15,7 +15,7 @@ var app = express();              // Do Not Edit
 
 // [Helmet](https://github.com/helmetjs/helmet) helps you secure your
 // Express apps by setting various HTTP headers.
-// Install the package, then require it.
+const helmet = require('helmet');
 
 
 
